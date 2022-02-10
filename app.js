e140d83c1fda09e874b054a6d55d344c6fbf11df
Anand Tripathi <shadarsh23@gmail.com>
2022-02-11 01:14:18 +0530

    added task addition and deletion functions

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 document.querySelector('#push').addEventListener('click', () => {
+    change();
+})
+
+document.querySelector('#newtask input').addEventListener('keypress', (event) => {
+    if (event.key == 'Enter')
+        document.querySelector('#push').click();
+})
+
+const change = function () {
     if (document.querySelector('#newtask input').value.length == 0) {
-        alert('Please Enter a Task');
+
     }
     else {
         document.querySelector('#tasks').innerHTML +=
@@ -10,9 +19,23 @@ document.querySelector('#push').addEventListener('click', () => {
                     ${document.querySelector('#newtask input').value}
                 </span> 
                 <button class="delete">
-                <i class="far fa-trash-alt"></i>
+                delete
                 </button>
             </div > 
             `;
+
+        let currentTasks = document.querySelectorAll('.delete');
+        for (let i = 0; i < currentTasks.length; i++) {
+            currentTasks[i].addEventListener('click', function () {
+                this.parentNode.remove();
+            })
+        }
+        let tasks = document.querySelectorAll('.task span');
+        for (let i = 0; i < tasks.length; i++) {
+            tasks[i].addEventListener('click', function () {
+                this.classList.toggle('completed');
+            })
+        }
     }
-})
+    document.querySelector('#newtask input').value = "";
+}
